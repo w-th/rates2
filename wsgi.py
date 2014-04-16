e4ec1d77656e561be72abe455caa94786bc19cc3
xczeratul <xczeratul@gmail.com>
2014-04-16 22:41:36 +0800

    fix len

--- wsgi.py
+++ wsgi.py
@@ -108,7 +108,8 @@ def application(environ, start_response):
         response_body = parse()
 
     status = '200 OK'
-    response_headers = [('Content-Type', ctype), ('Content-Length', str(len(response_body)))]
+    response_headers = [('Content-Type', ctype),
+        ('Content-Length', str(len(response_body.encode('utf-8'))))]
     #
     start_response(status, response_headers)
     return [response_body.encode('utf-8') ]
